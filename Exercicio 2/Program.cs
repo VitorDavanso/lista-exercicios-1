94750ba3febb798ad38e732ab47ccb5d96ce0342
vitor davanso <vitorciadavanso@gmail.com>
2022-03-02 22:06:45 -0300

    exercio 3

--- Exercicio 2/Program.cs
+++ Exercicio 2/Program.cs
@@ -6,7 +6,18 @@ namespace Exercicio_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ola Querido!");
+            static void Main ()
+            {
+                Console.Write("Valor em reais ?");
+                float reais = float.Parse(Console.ReadLine());
+
+                double usd = reais / 5.22;
+
+                Console.WriteLine("$ = " + usd);
+
+            }
+
         }
     }
 }
---
+++ exercio.tres/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace exercio.tres
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
